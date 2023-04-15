5e7dcdf320268e5eccd4e419ec721433484a6068
AdamBures <47189693+AdamBures@users.noreply.github.com>
2023-04-15 21:45:30 +0200

    Update views.py

--- LiquidLife/main/views.py
+++ LiquidLife/main/views.py
@@ -1,5 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
 	return render(request, "index.html")
+
+def data(request):
+	url = "https://epi.yale.edu/epi-results/2020/component/uwd"
+	response = requests.get(url)
+
+	# Parse the HTML content using BeautifulSoup
+	soup = BeautifulSoup(response.content, 'html.parser')
+
+	# Find the table element
+	table = soup.find('table')
+	tr_element = table.find_all('tr')[1:]
+	lst = []
+	# Loop through each row of the table
+	for row in tr_element:
+		a_element = row.find("td").find("a")
+		rank = row.find('td', {'class': 'epi-scorerank'}).text
+		value = row.find('td', {'class': 'epi-scorevalue'}).text
+		change = row.find('td', {'class': 'epi-scorechange'}).text
+		# Access the text content and href attribute of the a element
+		country_name = a_element.text
+		country_url = a_element['href']
+		url = "https://epi.yale.edu/" + country_url
+
+		dic = {
+			"country": country_name.strip(),
+			"country_url": country_url.strip(),
+			"rank": rank.strip(),
+			"value": value.strip(),
+			"change": change.strip()
+		}
+		lst.append(dic)
+	return JsonResponse({"data": lst})
+
+def info(request):
+	return render(request, 'info.html')
