5e38e8d35a75d5cbd0f7affa7f3a6020f817b9bd
tolujohn <tolujohnofficial@gmail.com>
2023-04-15 22:32:22 +0100

    css updated- img display

--- LiquidLife/static/javascript/main.js
+++ LiquidLife/static/javascript/main.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
+// $(document).ready(function(){
+//   $("a").on('click', function(event) {
 
-    if (this.hash !== "") {
-      event.preventDefault();
+//     if (this.hash !== "") {
+//       event.preventDefault();
 
-      var hash = this.hash;
+//       var hash = this.hash;
 
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 800, function(){
    
-        window.location.hash = hash;
-      });
-    }
-  });
-});
+//         window.location.hash = hash;
+//       });
+//     }
+//   });
+// });
 
 /* main js */
 (function() {
