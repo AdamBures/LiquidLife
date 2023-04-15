1d53e87ce436bb96b1d849ba1c5ff0b29099d2b2
AdamBures <47189693+AdamBures@users.noreply.github.com>
2023-04-15 23:43:20 +0200

    Update models.py

--- LiquidLife/main/models.py
+++ LiquidLife/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Filter(models.Model):
+	name = models.CharField(max_length=30)
+	icon = models.ImageField(upload_to ='static/images/')
+	description = models.TextField()
+	filtration_capacity = models.CharField(max_length=30)
+	filtration_rate = models.CharField(max_length=30)
+	micron_rating = models.CharField(max_length=30)
+	lifespan = models.CharField(max_length=30)
